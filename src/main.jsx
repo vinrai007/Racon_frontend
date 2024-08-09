e7f25ba36aeb5d76ea3c32d580020e00d9cdb5f3
vinrai007 <129852101+vinrai007@users.noreply.github.com>
2024-08-09 18:14:01 +0530

    11th

--- src/main.jsx
+++ src/main.jsx
@@ -1,58 +1,7 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Homepage from "./routes/homepage/Homepage";
-// import DashboardPage from "./routes/dashboardPage/DashboardPage";
-// import ChatPage from "./routes/chatPage/ChatPage";
-// import RootLayout from "./layouts/rootLayout/RootLayout";
-// import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
-// import SignInPage from "./routes/signInPage/signInPage";
-// import SignUpPage from "./routes/signUpPage/signUpPage";
-
-// const router = createBrowserRouter([
-//   {
-//     element: <RootLayout />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <Homepage />,
-//       },
-//       {
-//         path: "/sign-in/*",
-//         element: <SignInPage />,
-//       },
-//       {
-//         path: "/sign-up/*",
-//         element: <SignUpPage />,
-//       },
-//       {
-//         element: <DashboardLayout />,
-//         children: [
-//           {
-//             path: "/dashboard",
-//             element: <DashboardPage />,
-//           },
-//           {
-//             path: "/dashboard/chats/:id",
-//             element: <ChatPage />,
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ]);
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
-
 import React from "react";
-import "./index.css";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./routes/homepage/Homepage";
 import DashboardPage from "./routes/dashboardPage/DashboardPage";
 import ChatPage from "./routes/chatPage/ChatPage";
@@ -61,20 +10,71 @@ import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
 import SignInPage from "./routes/signInPage/signInPage";
 import SignUpPage from "./routes/signUpPage/signUpPage";
 
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/sign-in/*",
+        element: <SignInPage />,
+      },
+      {
+        path: "/sign-up/*",
+        element: <SignUpPage />,
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <DashboardPage />,
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<RootLayout />}>
-          <Route index element={<Homepage />} />
-          <Route path="/sign-in/*" element={<SignInPage />} />
-          <Route path="/sign-up/*" element={<SignUpPage />} />
-          <Route path="/dashboard" element={<DashboardLayout />}>
-            <Route index element={<DashboardPage />} />
-            <Route path="chats/:id" element={<ChatPage />} />
-          </Route>
-        </Route>
-      </Routes>
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+// import React from "react";
+// import "./index.css";
+// import ReactDOM from "react-dom/client";
+// import { HashRouter, Routes, Route } from "react-router-dom";
+// import Homepage from "./routes/homepage/Homepage";
+// import DashboardPage from "./routes/dashboardPage/DashboardPage";
+// import ChatPage from "./routes/chatPage/ChatPage";
+// import RootLayout from "./layouts/rootLayout/RootLayout";
+// import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
+// import SignInPage from "./routes/signInPage/signInPage";
+// import SignUpPage from "./routes/signUpPage/signUpPage";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       <Routes>
+//         <Route path="/" element={<RootLayout />}>
+//           <Route index element={<Homepage />} />
+//           <Route path="/sign-in/*" element={<SignInPage />} />
+//           <Route path="/sign-up/*" element={<SignUpPage />} />
+//           <Route path="/dashboard" element={<DashboardLayout />}>
+//             <Route index element={<DashboardPage />} />
+//             <Route path="chats/:id" element={<ChatPage />} />
+//           </Route>
+//         </Route>
+//       </Routes>
+//     </HashRouter>
+//   </React.StrictMode>
+// );
